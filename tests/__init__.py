ff4d0952c7dfb4398c3b444e15b7088f8c339ccf
Ellie Yampolskaya <ellie@lula.is>
2023-12-13 00:07:24 -0500

    class 11: part 2

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 BROWSER = 'chrome'
 DEFAULT_WAIT = 5
 
@@ -6,3 +8,10 @@ BASE_URL = f'{DOMAIN}/symfony/web/index.php'
 
 ADMIN_USER = 'admin'
 PASSWORD = 'password'
+
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(TEST_DIR)
+TEST_RESULTS = os.path.join(PROJECT_DIR, 'test_results')
+
+
